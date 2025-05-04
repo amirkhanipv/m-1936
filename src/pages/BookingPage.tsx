81d63b0c1e4e5ba1b664daa821d754de10b443e2
amirkhanipv <120177905+amirkhanipv@users.noreply.github.com>
2025-05-05 02:26:45 +0330

    Update BookingPage.tsx

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const BookingPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Booking Page</h1>
+      {/* محتوای صفحه رزرو */}
+    </div>
+  );
+};
+
+export default BookingPage;
